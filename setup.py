1d36d265dcc27ccbeacc44d05e606e244f09fee5
Horacio G. de Oro <hgdeoro@gmail.com>
2015-08-05 03:32:31 -0300

    Bump version - now at v0.0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='pcassandra',
-    version='0.0.4.dev',
+    version='0.0.5',
     packages=[
         'pcassandra',
         'pcassandra.dj18',
